5fc1eaff205055c538ac296f53da908b99c7fc5d
Nimrod <nimrodors@gmail.com>
2023-07-04 14:01:21 +0200

    delete HEAD

--- src/gitpractice/Car.java
+++ src/gitpractice/Car.java
@@ -5,12 +5,10 @@ public class Car {
 	private int wheel;
 	private int gear;
 	private int speed;
-<<<<<<< HEAD
 	
 	public Car() {
 		super();
 	}
-=======
->>>>>>> 28f4f0ee149c0ab80ebb63b31ab83ebc7766661c
+
 
 }
