1c9b3b76d93b8e49e09dd7321fb8e11f763b2429
Nimrod <nimrodors@gmail.com>
2023-07-04 16:55:58 +0200

    ki kell torolni

--- src/gitpractice/Main.java
+++ src/gitpractice/Main.java
@@ -8,11 +8,7 @@ public class Main {
 		
 		int b = 2;
 
-		System.out.println("Ez a bugfix");
-		
-		System.out.println("ezt most a masteren csinalom es ezt nem lehet kitorolni");
-		
-		System.out.println("ez a rebase elott");
+		System.out.println("Ezt elcsesztem fejlesztés, de nagyon!!!");
 	}
 
 }
